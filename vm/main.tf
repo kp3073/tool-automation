f264959da5f69f56784b4b2e28a4a6b2a6380ec6
kp3073 <keyurpatel8288@gmail.com>
2024-12-31 15:41:44 -0500

    roboshop terraform | az infra test

--- vm/main.tf
+++ vm/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_public_ip" "main" {
   resource_group_name = data.azurerm_resource_group.main.name
   location            = data.azurerm_resource_group.main.location
   allocation_method   = "Dynamic"
-  sku = "basic"
+  
 
   tags = {
 	environment = var.component

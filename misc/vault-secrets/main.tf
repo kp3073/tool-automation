4830b1e6697e6097df901b5eeb667370f73e9dad
kp3073 <keyurpatel8288@gmail.com>
2025-01-03 23:12:54 -0500

    roboshop vault

---
+++ misc/vault-secrets/main.tf
@@ -0,0 +1,15 @@
+resource "vault_mount" "main" {
+  for_each = var.secrets
+  path        = each.key
+  type        = "kv"
+  options = { version = "1" }
+  description = each.key
+}
+
+variable "secrets" {
+  default = {
+	infra = {}
+	roboshop = {}
+  }
+}
+
---
+++ misc/vault-secrets/provider.tf
@@ -0,0 +1,11 @@
+provider "vault" {
+  address = "http://vault.cloudaws.shop:8200"
+  token   = var.token
+}
+
+variable "token" {}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "4b236e6d-2c9a-4cb2-90a2-30a5377d8eb2"
+}

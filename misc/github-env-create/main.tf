8a8bb3d172bf65486f636e1a0cbaf64086faff50
kp3073 <keyurpatel8288@gmail.com>
2025-02-22 13:54:49 -0500

    tools-automation - change instance type

---
+++ misc/github-env-create/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+	github = {
+	  source  = "integrations/github"
+	  version = "6.5.0"
+	}
+  }
+}
+
+provider "github" {
+  # Configuration options
+  owner = "azawsdevops"
+}
+
+data "github_user" "current" {
+  username = "kp3073"
+}
+
+variable "repos" {
+  default = ["roboshop-cart", "roboshop-user", "roboshop-shipping","roboshop-payment", "roboshop-frontend"]
+}
+
+resource "github_repository_environment" "example" {
+  count = length(var.repos)
+  environment         = DEV
+  repository          = var.repos[count.index]
+  prevent_self_review = false
+  reviewers {
+	users = [data.github_user.current.id]
+  }
+}
